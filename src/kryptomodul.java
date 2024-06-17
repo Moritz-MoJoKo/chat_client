9895f84371e18016d5bc9bfe7e6b87d549a8b629
Moritz MoJoKo <100783353+Moritz-MoJoKo@users.noreply.github.com>
2024-06-17 21:29:53 +0200

    kryptomodul added

---
+++ src/kryptomodul.java
@@ -0,0 +1,18 @@
+
+/**
+ * Beschreiben Sie hier die Klasse kryptomodul.
+ * 
+ * @author Moritz Koch
+ * @version 13.06.2024
+ */
+public interface kryptomodul
+{
+
+    public void verschluesseln(String klartext, int key);
+    
+    public void entschluesseln(String geheimtext, int key);
+    
+    public void savekey(int key);
+    
+    public void loadkey();
+}
